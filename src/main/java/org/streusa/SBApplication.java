7ffb183fb62ba4d86d4887463e51871b0d6ba55c
Francesco Stumpo <FrancescoStumpo@MBPdiFrancesco.Home>
2023-04-16 15:39:27 +0200

    Overriding configure method in main class

--- src/main/java/org/streusa/SBApplication.java
+++ src/main/java/org/streusa/SBApplication.java
@@ -2,12 +2,17 @@ package org.streusa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.streusa.services.DatabaseService;
 
 @SpringBootApplication(scanBasePackages = {"org.streusa"})
 public class SBApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SBApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SBApplication.class, args);
