a88a8f1d2e9e0942c0cfdb127c7b5a84ec68b1b2
Francesco Stumpo <FrancescoStumpo@MBPdiFrancesco.Home>
2023-04-16 15:29:47 +0200

    Added tomcat dependency

--- src/main/java/org/streusa/SBApplication.java
+++ src/main/java/org/streusa/SBApplication.java
@@ -3,10 +3,11 @@ package org.streusa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.streusa.services.DatabaseService;
 
 @SpringBootApplication(scanBasePackages = {"org.streusa"})
-public class SBApplication {
+public class SBApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SBApplication.class, args);
